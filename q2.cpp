5f1f1aadc040576ca08c9e29411fad2d3c77bf43
hafza1912 <hafzaazath@gmail.com>
2025-09-18 13:27:47 +0530

    Add files via upload

---
+++ q2.cpp
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main(){
+	int a;
+	int b;
+	scanf("%d",&a);
+	scanf("%d",&b);
+	if (a<b){
+		printf("%d is smaller",a);
+	}
+	else {
+		printf("%d is smaller",b);
+	}
+	
+}
---
+++ q4.cpp
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main(){
+	int a;
+	int b;
+	scanf("%d",&a);
+	scanf("%d",&b);
+	if ((a-b)%2==0){
+		printf("difference is even");
+	}
+	else {
+		printf("difference is odd");
+	}
+	
+}
